dc7045905aea4ef41fca9b62ad9601bdebf142d1
Riccardo Campi <72938367+riccamper@users.noreply.github.com>
2025-10-29 10:09:06 +0100

    bench

--- energy_bench.py
+++ energy_bench.py
@@ -9,19 +9,19 @@ from private_settings import PRIVATE_SETINGS
 
 if __name__ == "__main__":
     # Load the dataset
-    dataset = pd.read_csv("./benchmark/backup/dataset_test.csv")
+    dataset = pd.read_csv("benchmark/backup/DatasetQA.csv")
 
     # Create the Guru instance
     if PRIVATE_SETINGS["LLM_LOCAL"]:
-        guru = Guru("ollama", "mistral", "nomic-embed-text", "english", 0, True)
+        guru = Guru("ollama", "gpt-oss:120b", "mxbai-embed-large", "english", 0, "Italy")
     else:
-        guru = Guru("openai", "gpt-4", "text-embedding-3-small", "english", 0, True)
+        guru = Guru("openai", "gpt-4", "text-embedding-3-small", "english", 0, "Italy")
 
     # Define the languages to benchmark
-    languages = ["IT", "EN"]
+    languages = ["Italiano", "English"]
 
     # Run the benchmark
-    result = Benchmark.run(guru, dataset, "Question - ", languages)
+    result = Benchmark.run(guru, dataset, "Question - ", languages, "Region")
 
     # Save the result to a CSV file
-    result.to_csv("benchmark/energy_benchmark.csv", index=False)
+    result.to_csv("benchmark/energy_benchmark.csv", columns=["Question - Italiano", "Answer - Italiano", "ANSWER_Italiano", "Question - English", "Answer - English", "ANSWER_English", "Source", "Region"], index=False)
